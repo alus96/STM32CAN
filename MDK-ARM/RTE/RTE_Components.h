d80fb07cc1a216158bd4817dfbca89e983dd356d
alus96 <sasha_c_yralmasha@mail.ru>
2016-08-31 04:26:06 +0300

    Initial commit

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32CAN' 
+ * Target:  'STM32CAN' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
